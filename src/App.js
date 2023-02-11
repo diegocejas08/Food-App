440fa1e2bd148c323088dc1f501dd81705168601
solitaM <soldemaciel@gmail.com>
2023-02-11 16:01:20 -0300

    se agregaron las rutas a contact y About y se comenzo a hacer responsive

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-
 // Estilos
 import './App.css';
 // Componentes
 // import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import About from './components/About';
+import Contact from './components/Contact'
 
 function App() {
   return (
     <>
-      
-      <Navbar/>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/About" element={<About/>} />
+        <Route path="/Contact" element={<Contact/>}/>
+        {/* <Route path='/' element= {Menú} /> */}
+      </Routes>
+      {/* <Header /> */}
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Logo from "../Navbar-img/Logo.webp";
-// import styles from "../index.css";
+// import { IconName } from "react-icons/fa";
 
 const Navbar = () => {
     return (
-        <>
+        <div className="sm">
             <nav className=" bg-yellow-100" >
-                <div className="flex justify-between items-center h-48 relative shadown-sm">
+                <div className="flex justify-between items-center h-48 relative shadown-sm ">
                     <img className="ml-4" src={Logo} alt="" />
-                    <ul className=" flex space-x-5">
-                        <li className="transform motion-safe:hover:scale-110  font-serif font-bold text-4xl text-green-600">Menú</li>
-                        <li className="transform motion-safe:hover:scale-110 font-serif font-bold text-4xl text-green-600">Nosotros</li>
-                        <li className="transform motion-safe:hover:scale-110 font-serif font-bold text-4xl text-green-600">Club Veggie</li>
-                        <li className="transform motion-safe:hover:scale-110 font-serif font-bold text-4xl text-green-600">Contactanos</li>
+                    <div>
+
+                    </div>
+
+                    <ul className=" md:flex hidden space-x-6">
+                        <li className="transform motion-safe:hover:scale-110  font-serif font-bold text-4xl text-green-600">
+                            <Link to={'/'}>Menú</Link>
+                        </li>
+                        <li className="transform motion-safe:hover:scale-110 font-serif font-bold text-4xl text-green-600">
+                            <Link to={'/About'}>Nosotros</Link>
+                        </li>
+                        <li className="transform motion-safe:hover:scale-110 font-serif font-bold text-4xl text-green-600">
+                            <Link to={'/'}>Home</Link>
+                        </li>
+                        <li className="transform motion-safe:hover:scale-110 font-serif font-bold text-4xl text-green-600">
+                            <Link to={'/Contact'}>Contacto</Link>
+                        </li>
                     </ul>
                 </div>
             </nav>
-        </>
+        </div>
 
     );
 };
